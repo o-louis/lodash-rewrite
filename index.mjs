31201acbf2237101792122217a911ed07dc314fa
o-louis <oriane.louis.pro@gmail.com>
2021-04-07 20:46:24 -0400

    feat(compact): Create compact function

--- index.mjs
+++ index.mjs
@@ -3,5 +3,8 @@ import * as $ from "./lib/index.mjs";
 // chunk
 const chunk1 = $.chunk(["a", "b", "c", "d"], 2);
 const chunk2 = $.chunk(["a", "b", "c", "d"], 3);
+console.log("// chunk //\n", { chunk1, chunk2 });
 
-console.log("// chunk //\t", { chunk1, chunk2 });
+// compact
+const compact1 = $.compact([0, null, 1, false, 2, "", 3, 0, 4]);
+console.log("// compact //\n", compact1);
