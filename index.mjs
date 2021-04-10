d0e1e3f34972beee4add42ef6fed44138581fcd2
o-louis <oriane.louis.pro@gmail.com>
2021-04-09 22:41:19 -0400

    feat(drop): Create drop function

--- index.mjs
+++ index.mjs
@@ -8,3 +8,10 @@ console.log("// chunk //\n", { chunk1, chunk2 });
 // compact
 const compact1 = $.compact([0, null, 1, false, 2, "", 3, 0, 4]);
 console.log("// compact //\n", compact1);
+
+// drop
+const drop1 = $.drop([1, 2, 3]);
+const drop2 = $.drop([1, 2, 3], 2);
+const drop3 = $.drop([1, 2, 3], 5);
+const drop4 = $.drop([1, 2, 3], 0);
+console.log("// drop //\n", { drop1, drop2, drop3, drop4 });
